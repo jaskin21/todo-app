b879ed23af261e21be4e9dfd3771121350272d51
jaskin <jaskin.agmata@gmail.com>
2022-04-05 23:47:41 +0800

    feat: added header and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-import { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
-  return <div className='App'>Homepage</div>;
+  return (
+    <main>
+      <Header />
+      <div className='main-container'>
+        <h1>HomePage</h1>
+      </div>
+      <Footer />
+    </main>
+  );
 }
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/Header.css';
+
+const Header = () => {
+  return (
+    <div className='header-container'>
+      <h1>Todo Task Manager</h1>
+    </div>
+  );
+};
+
+export default Header;
